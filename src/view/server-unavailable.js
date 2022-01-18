9ebea01da9781b7be3cc2396e9d0c967cae9fdae
Roman Katrovsky <katrovsky@gmail.com>
2022-01-18 06:06:58 +0300

    корректировка server-unavailable

--- src/view/server-unavailable.js
+++ src/view/server-unavailable.js
@@ -1,6 +1,6 @@
 import AbstractView from './abstract.js';
 
-const createServerAnavailableTemplate = () => (
+const createServerUnavailableTemplate = () => (
   '<p class="trip-events__msg" style="color: #ff0000; margin-top: 50px; margin-bottom: 50px;">Remote server is not available!</p>'
 );
 
@@ -11,6 +11,6 @@ export default class serverUnavailable extends AbstractView {
   }
 
   get template() {
-    return createServerAnavailableTemplate();
+    return createServerUnavailableTemplate();
   }
 }
